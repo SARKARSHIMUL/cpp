d69b92702c42dfec1dff715e458b76302eb1c40c
SHIMUL SARKAR <87913378+SARKARSHIMUL@users.noreply.github.com>
2022-04-29 17:07:17 +0530

    Update fibonacci_series.cpp

--- fibonacci_series.cpp
+++ fibonacci_series.cpp
@@ -1,1 +1,20 @@
-Welcome to shimulsarkar
+#include<iostream>
+using namespace std;
+
+int fib(int n ){
+    if (n<2)
+    {
+        return 1;
+    }
+    
+    return fib(n-2) + fib(n-1);
+
+}
+int main()
+{
+    int a;
+    cout<<"Enter a number :"<<endl;
+    cin>>a;
+    cout<<"the term in fibonaccin cequence at position "<<a<<" is "<<fib(a)<<endl;
+    return 0;
+}
